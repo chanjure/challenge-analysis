8944cd51e74578e863f2f7de67b7d716c59b5ef2
chanjure <chanjure@snu.ac.kr>
2023-03-29 11:22:21 +0100

    removed data; automate download

--- bin/run_analysis.sh
+++ bin/run_analysis.sh
@@ -1,3 +1,6 @@
+mkdir data
+curl -L -o data/survey.csv https://energydata.info/dataset/a27a9b60-706b-4c81-8608-c913d2ed998f/resource/fdef4f22-fe57-49b1-9c42-e5dac79cc90c/download/pakistanbiomassfieldsurvey.csv
+
 for order in 3 5 6
 do
   python bin/calc_fractal.py -o ${order}
